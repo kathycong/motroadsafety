4d17e7ef2f32d15a04d978a9f9c1c316425c6c8f
Kathlyn Ycong <kathlynycong@gmail.com>
2022-02-14 23:38:19 +1300

    updating functions

--- R/get_person_travelled.R
+++ R/get_person_travelled.R
@@ -1,38 +1,63 @@
 #' Getting the distance travelled by a traveller per polygon
 #'
 #' This function returns a dataframe of the total distance travelled by
-#' travellers  for each polygon
+#' travellers  for each polygon.
 #'
 #' @param routes The routes travelled by the traveller with the associated
-#' weight or number of travellers
+#' weight or number of travellers.
 #'
-#' @param polygon The polygon
+#' @param polygon An sfc class object. The polygon we are trying to intersect
+#' with teh routes.
+#'
+#' @param polygon_id A unique polygon identifier used for groupby and
+#' joining geometry data.
+#'
+#' @param weight The weight for each line segment or route. This is optional.
 
-get_person_travelled <- function(routes, polygon){
+get_person_travelled <- function(routes, weight, polygon, polygon_id){
 
-  # get or set relation_to_geometry attribute of an sf object to constant to
-  # to avoid any warnings
+  #weight is an optional argument
 
+  #if weight param is missing then default to 1
+  if(missing(weight)){
+    weight <- rep(1, nrow(routes))
+  } else weight
+
+
+  #############checks#########
+  # 1. input has the correct class
+  # 2. input has the correct length i.e. length(routes) == weight
+  # 3. Data has no NaNs
+
+  #binding routes and weight as its easier for aggregation later on
+  routes <- cbind(routes, weight = weight)
+
+  #merging polygon_id and polygon for ease in aggregation later on
+  polygon_id_df <- data.frame(polygon_id = polygon_id)
+  polygon <- st_sf(polygon_id_df, geometry = polygon)
+
+  #avoiding errors
   st_agr(routes) <- "constant"
   st_agr(polygon) <- "constant"
 
   # getting the intersection of polygons and routes provided
   output <- st_intersection(polygon, routes)
 
-  #total distance travelled times the number of traveler
-  output$total_dist <- (st_length((output)) * output$travelers)
+  #total distance travelled times the number of weight
+  output$total_dist <- (st_length((output)) * output$weight) #/1000 as recommended by simon to do this later to avoid machine truncation
+
   st_geometry(output) <- NULL
 
   #aggregating the total distance travelled by the traveller agains the polygon code
   output <- output %>%
-    group_by(SA22021_V1) %>% #this needs to be more generic as this code varies for different dataset
-    summarise(exposure_m = sum(total_dist), total_travelers = sum(travelers)) %>%
-    mutate(exposure_m = as.numeric(exposure_m))
+    group_by(polygon_id) %>%
+    summarise(person_travelled_km = sum(total_dist), total_weight = sum(weight)) %>%
+    mutate(person_travelled_km = as.numeric(person_travelled_km))
 
-  #adding polygon info
+  #adding geometric info
   output <- left_join(polygon, output)
 
   #returns only polygons that intersects with the routes, ensure no NAs are returned
-  output[which(!is.na(output$exposure_m)), ]
+  output[which(!is.na(output$person_travelled_km)), ]
 
 }
--- R/get_routes.R
+++ R/get_routes.R
@@ -9,19 +9,40 @@
 
 
 get_routes <- function(lat_source, lng_source, lat_dest, lng_dest, weight){
-  # check that the length of all inputs are equal
-  # weight parameter is not required, default is 1
+##note that gh::route uses the parameter "vehicle" to specify the transportation
+##mode
 
-  #get list of routes
+  ##Error handling on inputs
+  ##check that lengths for all inputs are the same
+  if (!all(sapply(list(length(lng_source), length(lat_dest),
+                       length(lng_dest)), FUN = identical,
+                  length(lat_source))))
+    stop("input lengths are not the same")
+
+  ##check that all input are numeric
+  if (!all(sapply(list(is.numeric(lng_source), is.numeric(lat_dest),
+                       is.numeric(lng_dest)), FUN = identical,
+                  is.numeric(lat_source))))
+    stop("inputs must be a numeric vector")
+
+  ##check that all inputs are not NAs
+  if (!all(sapply(list(is.na(lng_source), is.na(lat_dest),
+                       is.na(lng_dest)), FUN = identical,
+                  is.na(lat_source))))
+    stop("inputs must not contain NAs")
+
+
+  ##get list of routes
   routes_list <- lapply(seq.int(length(lat_source)), function(i) {
 
-    #change the route to an sf linesegment and bundle it into a list
+    ##change the route to an sf linesegment and bundle it into a list
     sf::st_as_sf(sf::st_sfc(sf::st_linestring(ghroute::route(lat_source[i],
                                                  lng_source[i],
                                                  lat_dest[i],
-                                                 lng_dest[i])[[1]][[1]][ ,2:1]), crs = 4326))})
+                                                 lng_dest[i])[[1]][[1]][ ,2:1]),
+                            crs = 4326))})
 
-  #transforms the nested list of sf linesegments or routes into a dataframe
+  ##transforms the nested list of sf linesegments or routes into a dataframe
   routes <- do.call(rbind, routes_list)
 
   cbind(routes, travelers = weight)
