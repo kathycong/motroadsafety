8ea716a7f5f3e68e303550021941be802bdd189c
kathycong <kathlynycong@gmail.com>
2022-02-25 22:52:59 +1300

    updating get_route functions and adding rmd file to test package functions

--- R/get_routes.R
+++ R/get_routes.R
@@ -15,9 +15,11 @@ get_routes <- function(lat_source, lng_source, lat_dest, lng_dest, osm_dir){
 
   if(missing(osm_dir)){
     if (!file.exists("new-zealand-latest.osm.pbf")){
-    download.file("https://download.geofabrik.de/australia-oceania/new-zealand-latest.osm.pbf", "new-zealand-latest.osm.pbf")
-      osm_dir <- "new-zealand-latest.osm.pbf"
-      }}
+      download.file("https://download.geofabrik.de/australia-oceania/new-zealand-latest.osm.pbf", "new-zealand-latest.osm.pbf")
+      warning("Downloading osm pbf file from https://download.geofabrik.de/australia-oceania/new-zealand-latest.osm.pbf")
+    }
+    osm_dir <- "new-zealand-latest.osm.pbf"   }
+
 
   ##Error handling on inputs
   ##check that lengths for all inputs are the same
@@ -39,7 +41,7 @@ get_routes <- function(lat_source, lng_source, lat_dest, lng_dest, osm_dir){
     stop("inputs must not contain NAs")
 
   #initialise router
-  gh:router(osm.file = osm_dir)
+  #ghroute::router(osm.file = osm_dir)
 
   ##get list of routes
   routes_list <- lapply(seq.int(length(lat_source)), function(i) {
@@ -54,5 +56,74 @@ get_routes <- function(lat_source, lng_source, lat_dest, lng_dest, osm_dir){
   ##transforms the nested list of sf linesegments or routes into a dataframe
   routes <- do.call(rbind, routes_list)
 
-  cbind(routes, travelers = weight)
+  routes
+  #cbind(routes, travelers = weight)
 }
+
+
+############# modified
+
+
+get_routes <- function(lat_source, lng_source, lat_dest, lng_dest, osm_dir){
+  ##note that gh::route uses the parameter "vehicle" to specify the transportation
+  ##mode
+
+
+  if(missing(osm_dir)){
+    if (!file.exists("new-zealand-latest.osm.pbf")){
+      download.file("https://download.geofabrik.de/australia-oceania/new-zealand-latest.osm.pbf", "new-zealand-latest.osm.pbf")
+      warning("Downloading osm pbf file from https://download.geofabrik.de/australia-oceania/new-zealand-latest.osm.pbf")
+    }
+    osm_dir <- "new-zealand-latest.osm.pbf"   }
+
+
+  ##Error handling on inputs
+  ##check that lengths for all inputs are the same
+  if (!all(sapply(list(length(lng_source), length(lat_dest),
+                       length(lng_dest)), FUN = identical,
+                  length(lat_source))))
+    stop("input lengths are not the same")
+
+  ##check that all input are numeric
+  if (!all(sapply(list(is.numeric(lng_source), is.numeric(lat_dest),
+                       is.numeric(lng_dest)), FUN = identical,
+                  is.numeric(lat_source))))
+    stop("inputs must be a numeric vector")
+
+  ##check that all inputs are not NAs
+  if (!all(sapply(list(is.na(lng_source), is.na(lat_dest),
+                       is.na(lng_dest)), FUN = identical,
+                  is.na(lat_source))))
+    stop("inputs must not contain NAs")
+
+  #initialise router
+  ghroute::router(osm.file = osm_dir)
+
+  ##get list of routes
+  routes_list <- lapply(seq.int(length(lat_source)), function(i) {
+
+    ##change the route to an sf linesegment and bundle it into a list
+    tryCatch(ghroute::route(lat_source[i],
+                                 lng_source[i],
+                                 lat_dest[i],
+                                 lng_dest[i])[[1]][[1]][ ,2:1],
+             error = function(e) NA)})
+
+  ##transforms the nested list of sf linesegments or routes into a dataframe
+
+  output <- purrr::map_dfr(routes_list, function(x){ st_as_sf(
+
+    if(any(is.na(x))){
+      st_sfc(x, crs = 4326)}
+    else {
+      st_sfc(st_linestring(x), crs = 4326)})
+  })
+
+  gc()
+ .jcall("java/lang/System", method = "gc")
+
+ output
+}
+
+
+
