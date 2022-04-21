988e57716e4d7fb7e3bfcd9c526ec6c027b07de3
“Kathlyn <kyco720@aucklanduni.ac.nz>
2022-04-21 22:31:53 +1200

    amending package and testing with full dataset

--- R/get_dist_travelled.R
+++ R/get_dist_travelled.R
@@ -9,37 +9,59 @@
 #' @param polygon An sfc class object. The polygon we are trying to intersect
 #' with teh routes.
 #'
-#' @param polygon_id A unique polygon identifier used for groupby and
-#' joining geometry data.
 #'
 #' @param weight The weight for each line segment or route. This is optional.
 
-get_dist_travel <- function(polygon, routes, weight){
+get_dist_travel <- function(polygon, routes, weight = NULL){
 
   #weight is an optional argument
 
+  ##check that polygon is an sf dataframe or sfc
+  if (!(inherits(polygon, "data.frame") & inherits(polygon, "sf")) | inherits(polygon, "sfc"))
+    stop("polygon input needs to be either an sf dataframe or sfc")
+
+  ##check that routes is an sf dataframe or sfc
+  if (!(inherits(routes, "data.frame") & inherits(routes, "sf")) | inherits(routes, "sfc"))
+    stop("routes input needs to be either an sf dataframe or sfc")
+
+  ##check that routes is an sfc LINESTRING
+  if (!(inherits(sf::st_geometry(routes), "sfc_LINESTRING")))
+    stop("routes need to be an sfc_LINESTRING")
+
+  ##check that polygon is is either an sfc_POLYGON or sfc_MULTIPOLYGON
+  if (!(inherits(sf::st_geometry(polygon), "sfc_MULTIPOLYGON") | inherits(sf::st_geometry(polygon), "sfc_POLYGON")))
+    stop("routes need to be an sfc_MULTIPOLYGON or sfc_POLYGON")
+
+  ##check that weight if not null is numeric
+  if (!is.null(weight) & !is.numeric(weight))
+    stop("weight needs to be numeric")
+
+  ##check that weight does not contain any NAs
+  if (!is.null(weight) & any(is.na(weight)))
+    stop("weight should not contain any NAs")
+
+  ##check that weight does not contain any NAs
+  if (!is.null(weight) & length(weight) != nrow(routes))
+    stop("weight and routes should have the same length or rows")
+
+
   #if weight param is missing then default to 1
-  if(missing(weight)){
+  if(is.null(weight)){
     weight <- rep(1, nrow(routes))
   } else weight
 
 
-  #############checks#########
-  # 1. input has the correct class
-  # 2. input has the correct length i.e. length(routes) == weight
-  # 3. Data has no NaNs
-
   #binding routes and weight as its easier for aggregation later on
-  routes <- cbind(routes, weight = weight)
+  routes <- cbind(routes, weight)
 
   #avoiding errors
-  sf::st_agr(routes) <- "constant"
-  sf::st_agr(polygon) <- "constant"
+  #sf::st_agr(routes) <- "constant"
+  #sf::st_agr(polygon) <- "constant"
 
   # getting the intersection of polygons and routes provided
   output <- sf::st_intersection(polygon, routes)
 
-  #getting distance travelled
+  #getting distance traveled
   output$total_dist<- sf::st_length((output)) * output$weight
 
   output <- sf::st_join(polygon, output[, c('total_dist', 'weight', 'geometry')])
@@ -50,11 +72,15 @@ get_dist_travel <- function(polygon, routes, weight){
     summarise(total_dist  = sum(total_dist),
               weight = sum(weight))
 
+  #tidying up sf dataframe
+  output <- sf::st_sf(output[!(names(output) %in% 'geometry')],
+                      geometry = output$geometry)
+
   return(output)
 
 
-  output %>% mapview(zcol = 'total_dist',
-          layer.name = "total distance travelled")
+  #output %>% mapview(zcol = 'total_dist',
+     #     layer.name = "total distance travelled")
 
   }
 
--- R/get_routes.R
+++ R/get_routes.R
@@ -1,73 +1,43 @@
 #' Getting the routes of multiple geographic coordinate system (lat/lon) points
 #'
-#' This function returns a dataframe of where each row an sf linesegment object from the lat/lon points
+#' This function returns a dataframe where each row is an sf line segment object
 #'
-#' @param lat_source The latitude coordinates of the source point, needs to be a vector
-#' @param lng_aource The longitude coodinates of the source point, needs to be a vector
-#' @param lat_dest The latitude coordinates of the destination point, needs to be a vector
-#' @param lng_dest The longitude coodinates of the destination point, needs to be a vector
-
-get_routes <- function(lat_source, lng_source, lat_dest, lng_dest, osm_dir = NULL){
-  ##note that gh::route uses the parameter "vehicle" to specify the transportation
-  ##mode
-
-  #checking NZ osm file is available
-  #if(missing(osm_dir)){
-  #  if (!file.exists("new-zealand-latest.osm.pbf")){
-   #   download.file("https://download.geofabrik.de/australia-oceania/new-zealand-latest.osm.pbf", "new-zealand-latest.osm.pbf")
-   #   warning("Downloading osm pbf file from https://download.geofabrik.de/australia-oceania/new-zealand-latest.osm.pbf")
-   # }
-   # osm_dir <- "new-zealand-latest.osm.pbf"   }
+#' @param matrix A 4 column matrix with the order start_latitude, start_longitude, end_latitude,
+#' end_longitude. Values in the matrix needs to be numeric.
+#' @param crs Target coordinate reference system: object of class 'crs', or input string for st_crs
 
 
-  ##Error handling on inputs
-  ##check that lengths for all inputs are the same
-  if (!all(sapply(list(length(lng_source), length(lat_dest),
-                       length(lng_dest)), FUN = identical,
-                  length(lat_source))))
-    stop("input lengths are not the same")
+get_routes <- function(matrix, crs = NULL){
 
-  ##check that all input are numeric
-  if (!all(sapply(list(is.numeric(lng_source), is.numeric(lat_dest),
-                       is.numeric(lng_dest)), FUN = identical,
-                  is.numeric(lat_source))))
-    stop("inputs must be a numeric vector")
+  ##error handling
 
-  ##check that all inputs are not NAs
-  if (!all(sapply(list(is.na(lng_source), is.na(lat_dest),
-                       is.na(lng_dest)), FUN = identical,
-                  is.na(lat_source))))
-    stop("inputs must not contain NAs")
+  ##need to check that router has been initialised
 
-  #initialise router
-  #ghroute::router(osm.file = osm_dir)
+  ##ensure that input is matrix
+  if (!inherits(matrix, "matrix") | !inherits(matrix, "array"))
+    stop("input needs to be a matrix")
 
-  ##get list of routes
-  routes_list <- lapply(seq.int(length(lat_source)), function(i) {
+  ##ensure that matrix is numeric
+  if (!is.numeric(matrix))
+    stop("matrix contains non-numeric value")
 
-    ##change the route to an sf linesegment and bundle it into a list
-    tryCatch(ghroute::route(lat_source[i],
-                                 lng_source[i],
-                                 lat_dest[i],
-                                 lng_dest[i])[[1]][[1]][ ,2:1],
-             error = function(e) NA)})
+  ##ensure that matrix has no NA values
+  if (any(is.na(matrix)))
+    stop("matrix contains NAs")
 
-  ##transforms the nested list of sf linesegments or routes into a dataframe
+  ## get the routes using ghroute
+  r <- ghroute::route(matrix, output = "sf")
+  sf::st_crs(r) = 4326
 
-  output <- purrr::map_dfr(routes_list, function(x){ sf::st_as_sf(
+  ## transform the crs if specified
+  if(!is.null(crs)){
+    r <- sf::st_transform(r, crs = crs)}
 
-    if(any(is.na(x))){
-      sf::st_sfc(x, crs = 4326)}
-    else {
-      sf::st_sfc(sf::st_linestring(x), crs = 4326)})
-  })
+  ## binding input matrix with routes and returns a dataframe
+  r <- cbind(r, as.data.frame(matrix))
 
-  #https://stackoverflow.com/questions/21937640/handling-java-lang-outofmemoryerror-when-writing-to-excel-from-r
- # gc()
-# rJava::.jcall("java/lang/System", method = "gc")
+  return(r)
 
- output
 }
-
 
 
